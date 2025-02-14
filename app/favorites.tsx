ab25fe568c9ee5b6468e3caa5dce89eb2c6e8c3e
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-02-14 16:57:47 +0200

    Reset the app and start creating tabs navigation

---
+++ app/favorites.tsx
@@ -0,0 +1,12 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  );
+};
+
+export default Page;
+const styles = StyleSheet.create({});
---
+++ app/films.tsx
@@ -0,0 +1,12 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>Films</Text>
+    </View>
+  );
+};
+
+export default Page;
+const styles = StyleSheet.create({});
